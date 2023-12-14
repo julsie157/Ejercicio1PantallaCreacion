a8074789ca2d2469c6c481637157b0d7800a3520
alvaroRodri23 <alvrod951@alu.centroafuera.es>
2023-12-14 14:02:16 +0100

    gnomopocho y spinnerRaza

--- app/src/main/java/com/example/personaje/MainActivity.kt
+++ app/src/main/java/com/example/personaje/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.personaje
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinnerRaza = findViewById<Spinner>(R.id.spinnerRaza)
+        val opcionesRaza = arrayOf("Humano","Elfo","Maldito","Enano")
+        spinnerRaza.adapter= ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,opcionesRaza)
+
     }
+
+
 }
