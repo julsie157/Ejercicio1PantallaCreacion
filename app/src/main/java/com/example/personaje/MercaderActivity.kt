a46bee1924179809837dd4c812b42f279b1a0a03
alvaroRodri23 <alvrod951@alu.centroafuera.es>
2024-01-23 11:10:10 +0100

    version mas renovada

--- app/src/main/java/com/example/personaje/MercaderActivity.kt
+++ app/src/main/java/com/example/personaje/MercaderActivity.kt
@@ -1,4 +1,23 @@
 package com.example.personaje
 
-class MercaderActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+class MercaderActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mercaderlayout)
+
+        val botonContinuarmercader: Button = findViewById(R.id.botonContinuarmercader)
+
+        botonContinuarmercader.setOnClickListener {
+            // Crear Intent y pasar a Maincontinuarmercader sin datos adicionales
+            val intent = Intent(this, Maincontinuarmercader::class.java)
+            startActivity(intent)
+        }
+    }
 }
